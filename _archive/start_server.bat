42f320400fbc042a2e30d4114e97d3955239fdbb
Chris Amato <accounts@knectar.com>
2025-12-08 09:51:14 -0500

    starting over

--- _archive/start_server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ============================================================
-echo Starting Flask Application
-echo ============================================================
-echo.
-cd /d %~dp0
-python app.py
-pause
